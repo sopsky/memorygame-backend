215733fd29e820bde8be0827d4f3ad7777b42239
Sigurdur Jonsson <siggijons29@gmail.com>
2023-06-07 22:15:59 +0200

    changes of get and post methods

--- index.js
+++ index.js
@@ -18,104 +18,125 @@ app.get("/", (req, res) => {
   res.json("hello this is the backend");
 });
 
-app.get("/topscore", (req, res) => {
-  const q = "SELECT * FROM scores";
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
-app.get("/topscore/:id", (req, res) => {
-  const id = req.params.id;
-  const q = `SELECT * FROM Scores WHERE score_id = ${id}`;
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
-
-app.post("/topscore", (req, res) => {
-  const q = "INSERT INTO Scores (`score`) VALUES (?)";
-  const values = [req.body.score];
-
-  db.query(q, values, (err, result) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    
-    // Get the inserted score_id
-    const scoreId = result.insertId;
-    
-    // Create the response object with the inserted score
-    const response = {
-      score_id: scoreId,
-      score: req.body.score
-    };
-
-    return res.status(200).json(response);
-  });
-});
-
-app.get("/userscore", (req, res) => {
-  const q = "SELECT * FROM UserScores";
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
-app.post("/userscore", (req, res) => {
-  const { user_id, score_id } = req.body;
-
-  const q = "INSERT INTO UserScores (user_id, score_id) VALUES (?, ?)";
-  const values = [user_id, score_id];
-
-  db.query(q, values, (err, result) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-
-    // Get the inserted userScore_id
-    const userScoreId = result.insertId;
-
-    // Create the response object with the inserted user score
-    const response = {
-      user_id: user_id,
-      score_id: score_id
-    };
-
-    return res.status(200).json(response);
-  });
-});
-
-
-
-app.get("/users", (req, res) => {
-  const q = "SELECT * FROM Users";
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
 app.post("/users", (req, res) => {
-  const q = "INSERT INTO Users (`username`) VALUES (?)";
-  const values = [req.body.username];
+  const { username, password } = req.body;
+
+  // Validate required fields
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  const q = "INSERT INTO Users (username, password) VALUES (?, ?)";
+  const values = [username, password];
 
   db.query(q, values, (err, result) => {
     if (err) return res.status(500).json({ error: "ERROR", details: err });
-    
+
     // Get the inserted user_id
     const userId = result.insertId;
-    
+
     // Create the response object with the inserted user
     const response = {
       user_id: userId,
-      username: req.body.username
+      username: username,
     };
 
     return res.status(200).json(response);
   });
 });
 
+app.get("/users", (req, res) => {
+  const q = "SELECT * FROM Users";
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).json({ error: "ERROR", details: err });
+
+    return res.status(200).json(data);
+  });
+});
+
+app.get("/usersid", (req, res) => {
+  const username = req.query.username; // Get the username from the query parameter
+
+  const q = `SELECT * FROM Users WHERE username = '${username}'`; // Add the username filter to the SQL query
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).json({ error: "ERROR", details: err });
+
+    return res.status(200).json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("connected to backend");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Validate required fields
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ error: "Username and password are required" });
+  }
+
+  const q = "SELECT * FROM Users WHERE username = ? AND password = ?";
+  const values = [username, password];
+
+  db.query(q, values, (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "ERROR", details: err });
+    }
+
+    if (data.length === 0) {
+      return res.status(401).json({ error: "Invalid username or password" });
+    }
+
+    // Login successful
+    return res.status(200).json({ message: "Login successful" });
+  });
+});
+
+app.post("/scores", (req, res) => {
+  const { score, user_id } = req.body;
+
+  // Validate required fields
+  if (!score || !user_id) {
+    return res.status(400).json({ error: "Score and user ID are required" });
+  }
+
+  const q = "INSERT INTO Scores (score, user_id) VALUES (?, ?)";
+  const values = [score, user_id];
+
+  db.query(q, values, (err, result) => {
+    if (err) return res.status(500).json({ error: "ERROR", details: err });
+
+    // Get the inserted score_id
+    const scoreId = result.insertId;
+
+    // Create the response object with the inserted score
+    const response = {
+      score_id: scoreId,
+      score: score,
+      user_id: user_id,
+    };
+
+    return res.status(200).json(response);
+  });
+});
+
+app.get("/scores", (req, res) => {
+  const q = "SELECT * FROM Scores";
+
+  db.query(q, (err, data) => {
+    if (err) return res.status(500).json({ error: "ERROR", details: err });
+
+    return res.status(200).json(data);
+  });
+});
 
 app.get("/pokemonimg", (req, res) => {
   const q = "SELECT * FROM pokemon_url";
@@ -133,31 +154,4 @@ app.post("/pokemonimg", (req, res) => {
     if (err) return res.status(500).json({ error: "ERROR", details: err });
     return res.status(200).json(data);
   });
-});
-
-app.delete("/topscore/:id", (req, res) => {
-  const scoreId = req.params.id;
-  const q = "DELETE FROM Scores WHERE score_id = ?";
-
-  db.query(q, [scoreId], (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
-
-app.delete("/users/:id", (req, res) => {
-  const scoreId = req.params.id;
-  const q = "DELETE FROM Users WHERE user_id = ?";
-
-  db.query(q, [scoreId], (err, data) => {
-    if (err) return res.status(500).json({ error: "ERROR", details: err });
-    return res.status(200).json(data);
-  });
-});
-
-
-
-app.listen(8800, () => {
-  console.log("connected to backend");
 });
